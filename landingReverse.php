31602a518fe819801bf378bd94410078d4deeebf
poojatr <poojatbabu9@gmail.com>
2017-10-19 18:25:42 +0530

    Add files via upload
    landing page for reverse coding

---
+++ landingReverse.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <title>Welcome to DeCoders</title>
+	<link rel="icon" href="../commonResources/img/title.png">
+    <?php require_once "../commonResources/includeCSS.php";?>
+	 <link href="../commonResources/css/um-template.min.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+<!-- Modal Login -->
+			<?php
+			//require_once 'connect.inc.php';
+			//session_start();
+			//include '../commonResources/modalLogin.inc.php';
+			?>
+<!-- Modal Login -->
+
+<!--Navigation-->
+			<?php require_once '../commonResources/navigation.inc.php';?>
+<!--/.Navigation-->
+
+
+<!--poster-->
+<body>
+  <!--Zoom effect-->
+  <div class="view overlay hm-zoom">
+      <img src="../commonResources/img/cover.jpg" class="img-fluid " alt="zoom">
+      <div class="mask flex-center waves-effect waves-light">
+      </div>
+  </div>
+
+
+  <marquee style="color:red">Last date to register is 10/11/2017 </marquee>
+
+
+
+  <div class="jumbotron container">
+
+    <!--  <img src="C:\xampp\htdocs\img\decoder.jpg" class="img-fluid center-block pagination-centered" width="350" height="350" >-->
+      <h1 class="h1-responsive">REVERSE CODING</h1>
+      <h1>Rules</h1>
+      <ul>
+        <li>mihbjnbh</li>
+        <li>mihbjnbh</li>
+        <li>mihbjnbh</li>
+        <li>mihbjnbh</li>
+        <li>mihbjnbh</li>
+      </ul>
+
+
+      <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention
+          to featured content or information.</p>
+      <hr class="my-2">
+      <p>It uses utility classes for typography and spacing to space content out within the larger
+          container.
+      </p>
+      <button type="button" class="btn btn-yellow">REGISTER</button>
+  </div>
