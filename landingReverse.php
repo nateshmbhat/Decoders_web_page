0be28ba645354917ee8684138736991b4affe279
poojatr <poojatbabu9@gmail.com>
2017-10-19 20:26:40 +0530

    many changes made

--- landingReverse.php
+++ landingReverse.php
@@ -45,13 +45,13 @@
 
 
 
-  <div class="jumbotron container">
+ <div class="jumbotron container">
 
-    <button class="btn btn-default btn-lg"><i class="zmdi zmdi-rotate-right zmdi-hc-spin"></i></button>
+    <!-- <button class="btn btn-default btn-lg"><i class="zmdi zmdi-rotate-right zmdi-hc-spin"></i></button>
     <button class="btn btn-default btn-lg"><i class="zmdi zmdi-rotate-left zmdi-hc-spin-reverse"></i></button>
       <h1 class="h1-responsive text-center">REVERSE CODING</h1>
 
-      <p class="text-left">DeCoders - the programming club of SIT is cming up with its event "REVERSE CODING" on 12th november.Its open for all four years.</p>
+      <p class="text-left">DeCoders - the programming club of SIT is coming up with its event "REVERSE CODING" on 12th november.Its open for all four years.</p>
       <h1>Rules</h1>
       <ul>
         <li>mihbjnbh</li>
@@ -69,7 +69,100 @@
           container.
       </p>
       <button type="button" class="btn btn-yellow">REGISTER</button>
-  </div>
+  </div> -->
+
+
+
+  <!--main content-->
+							<div class="main-content">
+								<div class="container">
+									<div class="header-title">
+										<!-- <h2 class="h2-responsive">inSight 2K17<a href="https://www.facebook.com/Sit.Decoders" target="_blank"><img src="../commonResources/img/fb.png" class="fb-icon"></a></h2> -->
+                      <h2 class="h2-responsive text-center ">REVERSE CODING</h2>
+									</div>
+									<div class="row">
+									<div class="col-md-6">
+										<h4 class="h4-responsive question" style="font-family:Arial;">What is Reverse Coding?</h4>
+											<p style="line-height:130% ;font-size:1.1rem;">Reverse coding is an innovative coding competation which tests the logical  thinking and basic programming skills.We will have 2 rounds .The first round will be a MCQ round and the second round will be a hands on coding round ,where the participants will be given an executable program and are asked to reverse coding,that is to develop the source code.
+                      </p>
+
+
+
+									</div>
+									<div class="col-md-6">
+										<h4 class="h4-responsive question" style="font-family:Arial;">Prizes</h4>
+											    <img src="../commonResources/img/1st.png" style="width:30%">
+												<img src="../commonResources/img/2nd.png" style="width:30%">
+												<img src="../commonResources/img/3rd.png" style="width:30%">
+										<p style="line-height:130% ;font-size:1.1rem; color:green">*Participation certificate to each participant.</p>
+
+									</div>
+									</div>
+
+										<h4 class="h4-responsive question" style="font-family:Arial;">General Information</h4>
+												<table class="table table-striped" style="font-size:1.1rem;">
+													<tr>
+														<td>Date</td><td>11th November, 2017.</td>
+													</tr>
+
+													<tr>
+														<td>Reporting time</td><td>01:30 PM.</td>
+													</tr>
+													<tr>
+														<td>Event start time</td><td>02:00 PM.</td>
+													</tr>
+													<tr>
+														<td>Event end time</td><td>06:00 PM.</td>
+													</tr>
+                          <tr>
+														<td>Round 1</td><td>02:00 PM to 03:00 PM.</td>
+													</tr>
+                          <tr>
+														<td>Round 2</td><td>03:30 PM to 5:00 PM.</td>
+													</tr>
+													<tr>
+														<td>Venue</td><td>Media Center,Siddaganga Institute of Technology,Tumakuru.</td>
+													</tr>
+													<tr>
+														<td>Participants</td><td>In a team of 2</td>
+													</tr>
+													<tr>
+														<td>Registration Fee </td><td>Free of Cost</td>
+													</tr>
+													<tr>
+														<td>Registration end date</td><td>10th November, 2017(11:59PM)</td>
+													</tr>
+												</table>
+										<h4 class="h4-responsive question" style="font-family:Arial;">Event & registration details</h4>
+											<ol style="font-size:1.1rem;">
+												<li style="list-style-type: circle">Registrations has to be done in a team of 2.</li>
+												<li style="list-style-type: circle">Each team should be headed by a team leader.</li>
+                        <li style="list-style-type: circle">The selected teams will only can particicpate in the 2nd round.</li>
+
+												<li style="list-style-type: circle">No usage of internet while participating in the competation.
+													</li>
+                          <li style="list-style-type: circle">Plaigarism of Code is not encouraged.
+  													</li>
+                          <li style="list-style-type: circle">The teams will be disqualified if any kind of cheating is found.
+  													</li>
+												<li style="list-style-type: circle">Decisions by our team will be final.</li>
+
+												<li style="list-style-type: circle; color:red;"><b>Please download the detailed instructions about the event from the link below.</b></li>
+												<!-- <li style="list-style-type: circle">You can register and view the problem statemets in the link below.</li> -->
+                        <!-- <li style="list-style-type: circle">We encourage students to submit proposals for more than one problems. This increases your chances of selection.</li> -->
+											</ol>
+										<br><br>
+										<center>
+										<a href="../commonResources/docs/instructions.pdf" download><button type="button" class="btn btn-primary">Download instructions</button></a>
+										<button type="button" class="btn btn-primary" onclick="location.href='../Problem';">Register</button>
+                  <!--  <button type="button" class="btn btn-primary" onclick="location.href='indexTemp.php';">View problem statements and register</button> -->
+										</center>
+
+
+								</div>
+							</div>
+            </div>
+			<!--main content-->
 
 
   <!--footer start-->
