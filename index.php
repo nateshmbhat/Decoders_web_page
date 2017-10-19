34c3e8d6ddc15700716eb4055a07f6c78897631c
nateshmbhat <nateshmbhat1@gmail.com>
2017-10-19 22:37:32 +0530

    index.php file will now receive all the form elements and put them in their respective variables.

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php 
 
-echo $_POST ;
+// ALL DETAILS of FORM ARE FETCHED INTO THESE VARIABLES.
 
+	$mem1_email = $_POST['mem1_email'] ; 
+	$mem1_USN = $_POST['mem1_USN']; 
+	$mem1_contact = $_POST['mem1_contact'] ; 
+	$mem1_name = $_POST['mem1_name'] ;
+
+	$mem2_email = $_POST['mem2_email'] ; 
+	$mem2_USN = $_POST['mem2_USN']; 
+	$mem2_contact = $_POST['mem2_contact'] ; 
+	$mem2_name = $_POST['mem2_name'] ;
+
+//-->>
+
+
+
+	//EMAIL VALIDATION
+
+	
+
+
+
+
+	//-->>
  ?>
